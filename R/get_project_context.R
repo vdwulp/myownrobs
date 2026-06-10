4f4b915903f4a71220d4795a1ab5265f29206095
vdwulp <58190301+vdwulp@users.noreply.github.com>
2026-06-10 11:10:39 +0200

    Revert changes

--- R/get_project_context.R
+++ R/get_project_context.R
@@ -4,14 +4,16 @@
 #' includes details about the working directory, RStudio project, source editor context, and
 #' operating system.
 #'
+#' @importFrom rstudioapi getActiveProject getSourceEditorContext
+#'
 #' @keywords internal
 #'
 get_project_context <- function() {
   list(
     r_terminal_working_directory = getwd(), # The current working directory of the R terminal.
-    rstudio_active_project = ipc_call("getActiveProject"), # The path to the currently active RStudio project.
+    rstudio_active_project = getActiveProject(), # The path to the currently active RStudio project.
     # The path of the file currently open in the RStudio source editor.
-    rstudio_source_editor_context = ipc_call("getSourceEditorContext")$path,
+    rstudio_source_editor_context = getSourceEditorContext()$path,
     os_type = paste(.Platform$OS.type, Sys.info()[["sysname"]]), # The OS type and name.
     architecture = Sys.info()[["machine"]] # The machine architecture.
   )
